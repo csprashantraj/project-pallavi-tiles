7d0f3f6f82dfe3b079161a1c9749943e5d92afce
Prashant Raj <prashant77399@gmail.com>
2024-10-09 18:56:32 +0530

    check

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,6 +5,7 @@ function Home() {
         <div>
             <p class="text-9xl">Home Page</p>
             <p class="text-8xl">Hello soon this website will be build</p>
+            <p class="text-7xl">Please wait for the updates</p>
         </div>
     )
 }
