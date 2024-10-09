2c8d97e56955f461ddcd864db97a89f654b739b3
Prashant Raj <prashant77399@gmail.com>
2024-10-09 18:49:53 +0530

    experimenting

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ function Home() {
     return (
         <div>
             <p class="text-9xl">Home Page</p>
+            <p class="text-8xl">Hello soon this website will be build</p>
         </div>
     )
 }
