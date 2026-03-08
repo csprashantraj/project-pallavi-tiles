a83cdb86a71cd1b1a58206c454db05e67c879083
Prashant Raj <prashant77399@gmail.com>
2026-03-09 00:03:41 +0530

    changing root

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/project-pallavi-tiles/',
   plugins: [react()],
   build: {
     // Target ES2015 so react-snap's bundled Chromium can execute the JS
